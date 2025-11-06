bbac87b6ba08b9c97065f9237cd8e7148dae678c
hlin152 <hlin152@asu.edu>
2025-11-06 07:28:04 -0700

    update live page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Varsity Sports Show",
+  title: "Varsity Sports Shows",
   description: "Testing Auth UI",
 };
 
