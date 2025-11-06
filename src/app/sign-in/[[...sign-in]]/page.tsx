553c65795849d8a55c9ec3c2812a203c7e53e0c9
hlin152 <hlin152@asu.edu>
2025-11-06 12:33:43 -0700

    Version 1.0

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="min-h-screen flex items-center justify-center p-6">
-      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" redirectUrl='/live'/>
+      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
     </div>
   );
 }
